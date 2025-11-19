f8b5369f11d6afef5d506c3a61705b827dd130f8
rohinvaidya <rohinvaidya@outlook.com>
2025-11-18 20:06:06 -0500

    frontend registration service  - deploy fix

--- frontend/src/lib/api/registrations.api.ts
+++ frontend/src/lib/api/registrations.api.ts
@@ -14,7 +14,7 @@ import {
   RegistrationStatus,
   RegisterForEventInput,
   CancelRegistrationInput,
-  CheckInInput,
+  CheckInAttendeeInput,
   ListRegistrationsInput,
   EventCapacityInfo,
   RegistrationStats,
